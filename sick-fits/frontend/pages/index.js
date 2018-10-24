88c847f541ccf3cbf57ed2c3829808f41ff9b571
jswiss <swissjoshua@gmail.com>
2018-10-24 17:53:57 +0200

    initial commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,15 @@
+// import React from 'react';
+
+// class Home extends React.Component {
+// 	render() {
+// 		return <p> Hey </p>;
+// 	}
+// }
+
+const Home = props => (
+	<div>
+		<p> Hey </p>
+	</div>
+);
+
+export default Home;
